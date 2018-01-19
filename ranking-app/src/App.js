b4eeb79380ead3fb0604b7daac9dfa361b25181c
mijakasia <katarzyna.nowak@rsqtechnologies.com>
2018-01-19 11:26:00 +0100

    react create app and base movies conf

---
+++ ranking-app/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('/movies')
+      .then(res => res.json())
+      .then(movies => this.setState({ movies }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Movies</h1>
+        {this.state.movies.map(movie =>
+          <div key={movie.id}>{movie.title}</div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
--- routes/movies.js
+++ routes/movies.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json([{
+  	id: 1,
+  	title: "movie 1"
+  }, {
+  	id: 2,
+  	title: "movie 2"
+  }]);
 });
 
 module.exports = router;
