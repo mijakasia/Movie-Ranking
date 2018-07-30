b770a093745cc536bb50f51b0c1f67670231c628
mijakasia <katarzyna.nowak@rsqtechnologies.com>
2018-07-30 19:38:25 +0200

    live reload react with express done

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/browser-refresh', function(req, res, next) {
+	if (process.env.NODE_ENV === 'dev') return res.redirect(process.env.BROWSER_REFRESH_URL)
+	return res.status(200).end();
+})
 
 module.exports = router;
