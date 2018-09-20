f5b7afc1ca6f1ffa7fc13115dd17e107cf6f7833
mijakasia <katarzyna.nowak@rsqtechnologies.com>
2018-09-20 18:50:56 +0200

    added props in constructor

--- ranking-app/src/containers/App.js
+++ ranking-app/src/containers/App.js
@@ -4,8 +4,8 @@ import '../styles/App.css';
 import fetchMovies from './apiCalls.js';
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       movies: [],
       errorStatus: '',
